016f07edad662ad3d1e268971776b76836467833
fiskar <artem@hismatullin.ru>
2020-02-24 00:10:23 +0500

    Done this lesson

--- src/App.js
+++ src/App.js
@@ -1,19 +1,66 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import TodoList from './Todo/TodoList'
+import Context from './context'
+import Loader from './Loader'
+import Modal from './Modal/Modal'
+
+const AddTodo = React.lazy(() => new Promise(resolve => {
+  setTimeout(() => {
+resolve(import('./Todo/AddTodo'))
+  }, 3000)
+}))
 
 function App() {
-  const todos = [
-    {id: 1, completed: false, title: 'Купить хлуб'},
-    {id: 2, completed: false, title: 'Купить масло'},
-    {id: 3, completed: false, title: 'Купить молоко'}
-  ]
+  const [todos, setTodos] = React.useState([])
+  const [loading, setLoading] = React.useState(true)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(todos => {
+        setTimeout(() => {
+          setTodos(todos)
+          setLoading(false)
+        }, 2000)
+      })
+  }, [])
+  
+  function toggleTodo(id) {
+    setTodos(todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    }))
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title) {
+    setTodos(todos.concat([{
+      title,
+      id: Date.now(),
+      completed: false
+    }]))
+  }
 
   return (
+    <Context.Provider value = {{ removeTodo }}>
     <div className='wrapper'>
       <h1>React tutorial</h1>
-
-      <TodoList todos={todos}></TodoList>
+      <Modal></Modal>
+      <React.Suspense fallback={<Loader />}>
+        <AddTodo onCreate={addTodo}></AddTodo>
+      </React.Suspense>
+      {loading && <Loader></Loader>}
+      {todos.length ?(<TodoList todos={todos} onToggle={toggleTodo}></TodoList>)
+: loading ? null : (
+ <p>No todos!</p>
+)}
     </div>
+    </Context.Provider>
   );
 }
 
---
+++ src/Loader.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default () => (
+<div style={{
+    display: 'flex',
+    justifyContent: 'center',
+    margin: '0',
+    padding: '0',
+    aliginItem: 'center',
+    minHigh: '100vh'
+    }
+    }>
+        <div className="pacman">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+    )
